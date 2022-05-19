5ac5fbd9f8a17839117b0b48879a32ad4d9ae4f2
Angel Wong <angel.wong@visionhq.io>
2022-05-19 10:16:42 +1000

    Delete unused code

--- app/student_portal/src/App.js
+++ app/student_portal/src/App.js
@@ -1,4 +1,3 @@
-import logo from './logo.svg';
 import './App.css';
 // Import Services
 import { checkHealth } from './service/api';
@@ -14,20 +13,6 @@ function App() {
   };
   return (
     <div className='App'>
-      <header className='App-header'>
-        <img src={logo} className='App-logo' alt='logo' />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className='App-link'
-          href='https://reactjs.org'
-          target='_blank'
-          rel='noopener noreferrer'
-        >
-          Learn React
-        </a>
-      </header>
       <button onClick={handleclick}>Click to check health</button>
     </div>
   );
