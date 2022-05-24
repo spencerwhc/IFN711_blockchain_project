1c33fd3f9e0604d2511a66c8fd6309d7a4aa61b0
spencerwhc <spencer.whc@gmail.com>
2022-05-25 04:10:52 +1000

    eslint tidyup

--- app/student_portal/src/components/NavTabs.js
+++ app/student_portal/src/components/NavTabs.js
@@ -1,48 +1,48 @@
-import * as React from 'react';
-import Box from '@mui/material/Box';
-import Tabs from '@mui/material/Tabs';
-import Tab from '@mui/material/Tab';
-import { Link } from 'react-router-dom';
-import '../styles/NavTabStyle.css';
-import { Typography } from '@mui/material';
+import * as React from "react";
+import Box from "@mui/material/Box";
+import Tabs from "@mui/material/Tabs";
+import Tab from "@mui/material/Tab";
+import { Link } from "react-router-dom";
+import "../styles/NavTabStyle.css";
+import { Typography } from "@mui/material";
 
 function LinkTab(props) {
-  return <Tab component={Link} to={props.pathname} {...props} />;
+    return <Tab component={Link} to={props.pathname} {...props} />;
 }
 
 export default function NavTabs() {
-  const [value, setValue] = React.useState(0);
+    const [value, setValue] = React.useState(0);
 
-  const handleChange = (event, newValue) => {
-    setValue(newValue);
-  };
+    const handleChange = (event, newValue) => {
+        setValue(newValue);
+    };
 
-  return (
-    <Box
-      sx={{
-        width: '100%',
-        bgcolor: '#001E36',
-        justifyContent: 'space-between',
-        display: 'flex'
-      }}
-    >
-      <Tabs
-        value={value}
-        onChange={handleChange}
-        TabIndicatorProps={{
-          style: {
-            display: 'none'
-          }
-        }}
-        aria-label='nav tabs example'
-      >
-        <LinkTab label='View Reports' pathname='/' />
-        <LinkTab label='Generate reports' pathname='/generate' />
-      </Tabs>
+    return (
+        <Box
+            sx={{
+                width: "100%",
+                bgcolor: "#001E36",
+                justifyContent: "space-between",
+                display: "flex",
+            }}
+        >
+            <Tabs
+                value={value}
+                onChange={handleChange}
+                TabIndicatorProps={{
+                    style: {
+                        display: "none",
+                    },
+                }}
+                aria-label="nav tabs example"
+            >
+                <LinkTab label="View Reports" pathname="/" />
+                <LinkTab label="Generate reports" pathname="/generate" />
+            </Tabs>
 
-      <Box sx={{ alignItems: 'center', m: '0.7rem' }}>
-        <Typography sx={{ color: '#fff' }}> Byrant Tanadjaya </Typography>
-      </Box>
-    </Box>
-  );
+            <Box sx={{ alignItems: "center", m: "0.7rem" }}>
+                <Typography sx={{ color: "#fff" }}> Ho Sze Wong </Typography>
+            </Box>
+        </Box>
+    );
 }
