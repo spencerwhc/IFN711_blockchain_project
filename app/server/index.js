5d7e56bdb08677dea44ffb0c5c5ce48039db80e6
spencerwhc <spencer.whc@gmail.com>
2022-05-28 21:53:06 +1000

    Combined

--- app/server/index.js
+++ app/server/index.js
@@ -1,14 +1,14 @@
 // Import libraries
-require('dotenv-flow').config({ node_env: process.env.DEPLOYMENT_PLATFORM });
-const path = require('path');
-const cors = require('cors');
-const express = require('express');
+require("dotenv-flow").config({ node_env: process.env.DEPLOYMENT_PLATFORM });
+const path = require("path");
+const cors = require("cors");
+const express = require("express");
 
 // Initialise an Express application
 const app = express();
 
 // Configure CORS settings
-const WHITELISTED_ORIGINS = ['http://localhost:3000', 'http://localhost:8080'];
+const WHITELISTED_ORIGINS = ["http://localhost:3000", "http://localhost:8080"];
 app.use(cors({ origin: WHITELISTED_ORIGINS, credentials: true }));
 
 // Add body parser middleware
@@ -16,16 +16,18 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 // Serve static assets
-app.use(express.static(path.join(__dirname, 'build')));
+app.use(express.static(path.join(__dirname, "build")));
 
-app.use('/api', require('./routes'));
+app.use("/api", require("./routes"));
+
+app.use(express.static(path.join(__dirname, "../student_portal/build")));
 
 // If none of the above matched, render client views
-// app.get('/*', (_, res) =>
-//   res.sendFile(path.join(__dirname, 'build', 'index.html'))
-// );
+app.get("/*", (_, res) =>
+    res.sendFile(path.join(__dirname, "../student_portal/build", "index.html"))
+);
 
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, async () => {
-  console.log(`App server listening on port: ${PORT}`);
+    console.log(`App server listening on port: ${PORT}`);
 });
--- app/student_portal/src/service/api.js
+++ app/student_portal/src/service/api.js
@@ -1,25 +1,25 @@
 import axios from "axios";
 
 export const checkHealth = async (data) => {
-    return await axios.get("http://localhost:8080/api/health", data);
+    return await axios.get("/api/health", data);
 };
 
 export const getReport = async (id) => {
-    return await axios.get(`http://localhost:8080/api/report/${id}`);
+    return await axios.get(`/api/report/${id}`);
 };
 
 export const postReport = async (data) => {
-    return await axios.post("http://localhost:8080/api/report/", data);
+    return await axios.post("/api/report/", data);
 };
 
 export const shareReport = async (data) => {
-    return await axios.post("http://localhost:8080/api/report/share", data);
+    return await axios.post("/api/report/share", data);
 };
 
 export const getStudent = async (id) => {
-    return await axios.get(`http://localhost:8080/api/student/${id}`);
+    return await axios.get(`/api/student/${id}`);
 };
 
 export const getStudentReports = async (id) => {
-    return await axios.get(`http://localhost:8080/api/student/${id}/reports`);
+    return await axios.get(`/api/student/${id}/reports`);
 };
