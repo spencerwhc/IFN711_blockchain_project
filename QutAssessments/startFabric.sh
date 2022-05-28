6214974b00282a8fc3ed7bd730d9730075ea34b6
spencerwhc <spencer.whc@gmail.com>
2022-05-29 00:08:15 +1000

    added script to run everything

--- QutAssessments/startFabric.sh
+++ QutAssessments/startFabric.sh
@@ -20,6 +20,11 @@ pushd ../test-network
 ./network.sh up createChannel -ca -s couchdb
 ./network.sh deployCC -ccn qut -ccv 1 -cci initLedger -ccl javascript -ccp ${CC_SRC_PATH}
 popd
+node enrollAdmin.js
+node registerUser.js
+pushd ../app/server
+npm start
+popd
 
 cat <<EOF
 
